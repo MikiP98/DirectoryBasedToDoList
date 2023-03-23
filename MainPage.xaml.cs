87e4eca299caed3d57b6679ac99b862ed1fa1b4b
MikiP98 <pokora.mikolaj@gmail.com>
2023-03-23 17:06:02 +0100

    Dodaj pliki projektów.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectoryBasedToDoList;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DirectoryBasedToDoList;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
